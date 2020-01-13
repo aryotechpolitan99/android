5fc2fc640c82f81ea2d7ba01b1d28458a19ee41c
shanon <shanonathooya@gmail.com>
2020-01-13 17:51:02 +0700

    onSaveInstanceSave (simpan state saat pindah dari potrait ke landscape)

--- app/src/main/java/com/aryotechpol/counter/MainActivity.java
+++ app/src/main/java/com/aryotechpol/counter/MainActivity.java
@@ -1,13 +1,16 @@
 package com.aryotechpol.counter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private final String STATE_COUNT = "state_count"; // konstanta
     TextView tvCount;
     Button btnCount,btnReset;
     int count = 0; // tipe data primitif (cara 1)
@@ -28,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // do something here
                 count++;
+                if(count == 10){
+                    tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.style_minimize));
+                }
                 tvCount.setText(String.valueOf(count)); // (cara 1)
                 //tvCount.setText(count.toString()) //(cara 2)
             }
@@ -37,9 +43,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 count = 0;
+                tvCount.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimensionPixelSize(R.dimen.default_view));
                 tvCount.setText(String.valueOf(count));
             }
         });
 
+        if(savedInstanceState != null){ // cek apakah ada state yang tersimpan?
+            count = savedInstanceState.getInt(STATE_COUNT);
+            tvCount.setText(String.valueOf(count));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save state
+
+        outState.putInt(STATE_COUNT,count);
     }
 }
